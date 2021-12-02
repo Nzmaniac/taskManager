0ffa19cb413b60a82bcf990565115188ad73e553
Dave Chapman <infiniteloopnz@gmail.com>
2021-12-03 10:25:27 +1300

    rollback of experimantal elements

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +7,12 @@ import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'Task Manager';
-  values: any;
 
-  constructor( private http: HttpClient) {}
+  constructor()  {}
  
   
   ngOnInit() {
-    this.http.get('http://localhost:3000/users').subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.log(error);
-    });
-  }
-    drop(event: CdkDragDrop<string[]>) {
-      moveItemInArray(this.values, event.previousIndex, event.currentIndex);
+   
     }
-  
+
 }
--- Ex/app.js
+++ Ex/app.js
@@ -1,28 +1,54 @@
-const cors = require('cors');
-
 const express = require('express')
+const cors = require('cors');
+const fs = require("fs");
+
+// setup express listening server
 const app = express()
 const port = 3000
 app.use(cors());
 app.options('*', cors());
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+//setup express to parse request body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+// handle endpoints
+// /post new user with timestamp of entry
+app.post('/register', (req, res) => {
+  var data = req.body
+
+  let today = new Date().toLocaleString();
+
+  var ws = fs.createWriteStream('users.txt', {flags: 'a'});
+  ws.write( 'Email: ' + data["Email"] + '\n')
+  ws.write( 'Password: ' + data["Password"] + '\n')
+  ws.write( 'Date: ' + today + '\n')
+  ws.end();
+
+  res.send('User registration received')
+})
 var users = [
     {name:'Dave', phone:'021914357', country:'New Zealand'},
     {name:'Sarah', phone:'021917730', country:'New Zealand'},
     {name:'Cody', phone:'021777555', country:'Prestons'},
     ]
-   
-app.get('/', (req, res) => {
-
-res.send(
-    )
-})
-
+// get all users from file users.txt
 app.get('/users', (req, res) => {
-
-  res.send(users)
-
+  fs.readFile("users.txt", "utf-8", (err, data) => {
+    res.send(data);
+  });
+    
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.get('/users', (req, res) => {
+
+//   res.send(users)
+
+// })
+
